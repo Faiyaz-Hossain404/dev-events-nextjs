cbe9fb061aaac24d4b55eb72a8e51215e592e46f
Fiayz <sifathhossain115@gmail.com>
2026-01-06 00:10:39 +0600

    Feat: sample event data viewable in UI

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,3 +1,22 @@
-export default function EventCard() {
-  return <div></div>;
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  title: string;
+  image: string;
+}
+
+export default function EventCard({ title, image }: Props) {
+  return (
+    <Link href={"/events"} id="event-card">
+      <Image
+        src={image}
+        alt={title}
+        width={410}
+        height={300}
+        className="poster"
+      />
+      <p>{title}</p>
+    </Link>
+  );
 }
