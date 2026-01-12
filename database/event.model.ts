9df00f924d4cf9bf079a64a19bd035cf491dae85
Fiayz <sifathhossain115@gmail.com>
2026-01-12 20:24:58 +0600

    Feat: EventSchema designed

--- database/event.model.ts
+++ database/event.model.ts
@@ -1,5 +1,6 @@
 import { Schema, Model, Document, Types } from "mongoose";
 
+//typescript interface for Event document
 export interface IEvent extends Document {
   title: string;
   slug: string;
@@ -18,3 +19,90 @@ export interface IEvent extends Document {
   createdAt: Date;
   updatedAt: Date;
 }
+
+const EventSchema = new Schema<IEvent>(
+  {
+    title: {
+      type: String,
+      required: [true, "Event title is required"],
+      trim: true,
+      maxlength: [100, "Title can not exceed 100 characters"],
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+      maxlength: [1000, "Description can not exceed 1000 characters"],
+    },
+    overview: {
+      type: String,
+      required: [true, "Overview is required"],
+      trim: true,
+      maxlength: [500, "Overview can not exceed 500 characters"],
+    },
+    image: {
+      type: String,
+      required: [true, "Image URL is required"],
+      trim: true,
+    },
+    venue: {
+      type: String,
+      required: [true, "Venue is required"],
+      trim: true,
+    },
+    location: {
+      type: String,
+      required: [true, "Location is required"],
+      trim: true,
+    },
+    date: {
+      type: String,
+      required: [true, "Date is required"],
+    },
+    time: {
+      type: String,
+      required: [true, "Time is required"],
+    },
+    mode: {
+      type: String,
+      required: [true, "Mode is required"],
+      enum: {
+        values: ["online", "offline", "hybrid"],
+        message: "Mode must be either online, offline, or hybrid",
+      },
+    },
+    audience: {
+      type: String,
+      required: [true, "Audience is required"],
+      trim: true,
+    },
+    agenda: {
+      type: [String],
+      required: [true, "Agenda is required"],
+      validate: {
+        validator: (v: string[]) => v.length > 0,
+        message: "Agenda must have at least one item",
+      },
+    },
+    organizer: {
+      type: String,
+      required: [true, "Organizer is required"],
+      trim: true,
+    },
+    tags: {
+      type: [String],
+      required: [true, "At least one tag is required"],
+      validate: {
+        validator: (v: string[]) => v.length > 0,
+        message: "There must be at least one tag",
+      },
+    },
+  },
+  { timestamps: true } //auto-generate createdAt and updatedAt fields
+);
